1c8e42d133206966edfb01f4d770db8441aa5a9c
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2025-02-16 18:39:45 +0200

    Created action.js file and addded the functions to increase , decrease and display the count

---
+++ actions.js
@@ -0,0 +1,19 @@
+const increase = (state) => {
+    return {
+        ...state,
+        count:state.count + 1
+    }
+}
+
+const decrease = (state) => {
+    return {
+        ...state,
+        count:state.count - 1
+    }
+}
+
+const get = (state, key) => {
+    return {
+        state[key]
+    }
+}
